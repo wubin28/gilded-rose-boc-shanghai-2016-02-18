8b02fa6364cfbe7dfd8b4de238ef233032e16dfd
snake_761BD652B7 <snake@cyber-dojo.org>
2016-02-12 04:33:56 +0000

    13

--- sandbox/GildedRoseTest.java
+++ sandbox/GildedRoseTest.java
@@ -101,5 +101,16 @@ public class GildedRoseTest {
         assertEquals(23, app.items[0].quality);
     }
 
+    @Test
+    public void Passes_quality_should_increate__by_1_when_sellin_bettwen_5_and_10() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 10, 22) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[0].name);
+        assertEquals(9, app.items[0].sellIn);
+        assertEquals(24, app.items[0].quality);
+    }
 
 }
