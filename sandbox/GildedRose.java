b9b9153e709b0fd5e4c8a73d3727f6a49ba362af
snake_761BD652B7 <snake@cyber-dojo.org>
2016-02-12 04:44:50 +0000

    17

--- sandbox/GildedRose.java
+++ sandbox/GildedRose.java
@@ -35,9 +35,9 @@ class GildedRose {
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+         
+            items[i].sellIn = items[i].sellIn - 1;
+            
 
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
