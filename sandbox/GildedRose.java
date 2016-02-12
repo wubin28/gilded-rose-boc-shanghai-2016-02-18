4e371ef8d2e9180afa3b43077712fe30a41d0d4f
snake_761BD652B7 <snake@cyber-dojo.org>
2016-02-12 04:42:27 +0000

    15

--- sandbox/GildedRose.java
+++ sandbox/GildedRose.java
@@ -7,6 +7,9 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
+            if(items[i].name.equals("Sulfuras, Hand of Ragnaros")){
+                continue;
+            }
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
