c2d15a3615834a6a0c99914085ffeffb71504e69
snake_761BD652B7 <snake@cyber-dojo.org>
2016-02-12 04:55:09 +0000

    25

--- sandbox/GildedRose.java
+++ sandbox/GildedRose.java
@@ -27,8 +27,7 @@ class GildedRose {
             }
 
 
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {                    
                     items[i].quality = items[i].quality - 1;
                 }
@@ -57,19 +56,15 @@ class GildedRose {
             
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {  
-                            items[i].quality = items[i].quality - 1;                           
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+         
+                if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (items[i].quality > 0) {  
+                        items[i].quality = items[i].quality - 1;                           
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+                    items[i].quality = items[i].quality - items[i].quality;
                 }
+                
             }
         }
     }
