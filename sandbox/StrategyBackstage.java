375250d1398ca46fa60ee1a9f8a6cc23a84fd4ef
snake_761BD652B7 <snake@cyber-dojo.org>
2016-02-12 05:56:18 +0000

    59

--- sandbox/StrategyBackstage.java
+++ sandbox/StrategyBackstage.java
@@ -1,8 +1,24 @@
 public class StrategyBackstage implements StrategyItem{
 
     public void handle(Item item){
-        if(item.name.equals("Aged Brie")){
-            return;
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+            if (item.sellIn < 11) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            }
+            if (item.sellIn < 6) {
+                if (item.quality < 50) {
+                    item.quality += 1;
+                }
+            }
+        }
+
+        item.sellIn -= 1;
+
+        if (item.sellIn < 0) {             
+            item.quality = item.quality - item.quality;                    
         }
     }
 }
