46a9e8b16dda399d822b9ca3ad02ae8123ae529d
snake_761BD652B7 <snake@cyber-dojo.org>
2016-02-12 04:44:27 +0000

    16

--- sandbox/GildedRose.java
+++ sandbox/GildedRose.java
@@ -12,10 +12,8 @@ class GildedRose {
             }
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
+                if (items[i].quality > 0) {                    
+                    items[i].quality = items[i].quality - 1;
                 }
             } else {
                 if (items[i].quality < 50) {
