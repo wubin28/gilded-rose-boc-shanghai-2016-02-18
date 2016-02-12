0cc7c849f9e565d3b1b260011b78d3defdececb7
snake_761BD652B7 <snake@cyber-dojo.org>
2016-02-12 05:59:24 +0000

    62

--- sandbox/GildedRose.java
+++ sandbox/GildedRose.java
@@ -7,10 +7,8 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-
             StrategyItem strategyItem = StrategyFactory.create(items[i].name);
             strategyItem.handle(items[i]);
-
         }
     }
 }
--- sandbox/StrategyAgedBrie.java
+++ sandbox/StrategyAgedBrie.java
@@ -2,15 +2,15 @@ public class StrategyAgedBrie implements StrategyItem{
 
     public void handle(Item item){
         if (item.quality < 50) {
-            item.quality = item.quality + 1;
+            item.quality += 1;
         }
 
-        item.sellIn = item.sellIn - 1;
+        item.sellIn -= 1;
     
         if (item.sellIn < 0) {
            
             if (item.quality < 50) {
-                item.quality = item.quality + 1;
+                item.quality += 1;
             }                   
         }
     }
