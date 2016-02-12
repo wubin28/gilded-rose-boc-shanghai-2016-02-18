5328300b96fd08f8518602002311f246c9b85cac
snake_761BD652B7 <snake@cyber-dojo.org>
2016-02-12 04:17:58 +0000

    8

--- sandbox/GildedRoseTest.java
+++ sandbox/GildedRoseTest.java
@@ -9,7 +9,9 @@ public class GildedRoseTest {
     public void normal_item_quality_should_decrease_by_1_if_sellin_greater_than_0() {
         Item[] items = new Item[] { new Item("Normal item", 8, 10) };
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
+
         assertEquals("Normal item", app.items[0].name);
         assertEquals(7, app.items[0].sellIn);
         assertEquals(9, app.items[0].quality);
@@ -19,7 +21,9 @@ public class GildedRoseTest {
     public void normal_item_quality_should_decrease_by_2_if_sellin_no_more_than_0() {
         Item[] items = new Item[] { new Item("Normal item", 0, 10) };
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
+
         assertEquals("Normal item", app.items[0].name);
         assertEquals(-1, app.items[0].sellIn);
         assertEquals(8, app.items[0].quality);
@@ -30,7 +34,9 @@ public class GildedRoseTest {
     public void Aged_Brie_quality_should_increase_by_1_if_sellin_greater_than_0() {
         Item[] items = new Item[] { new Item("Aged Brie", 8, 10) };
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
+
         assertEquals("Aged Brie", app.items[0].name);
         assertEquals(7, app.items[0].sellIn);
         assertEquals(11, app.items[0].quality);
@@ -39,7 +45,9 @@ public class GildedRoseTest {
     public void Aged_Brie_quality_should_increase_by_2_if_sellin_no_more_than_0() {
         Item[] items = new Item[] { new Item("Aged Brie", 0, 10) };
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
+
         assertEquals("Aged Brie", app.items[0].name);
         assertEquals(-1, app.items[0].sellIn);
         assertEquals(12, app.items[0].quality);
