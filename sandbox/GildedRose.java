7f40cbf9d958473ad2d64795fb5ec95bece76505
snake_761BD652B7 <snake@cyber-dojo.org>
2016-02-12 04:48:50 +0000

    20

--- sandbox/GildedRose.java
+++ sandbox/GildedRose.java
@@ -32,19 +32,11 @@ class GildedRose {
             
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {  
-                            items[i].quality = items[i].quality - 1;                           
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+               
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
                 }
+                
             }
                 continue;
             }
