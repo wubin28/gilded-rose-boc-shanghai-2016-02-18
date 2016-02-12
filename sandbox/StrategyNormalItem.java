706404e7d8b66832e2c8d5a2a374ffe105d96763
snake_761BD652B7 <snake@cyber-dojo.org>
2016-02-12 05:57:45 +0000

    60

--- sandbox/StrategyNormalItem.java
+++ sandbox/StrategyNormalItem.java
@@ -1,8 +1,17 @@
 public class StrategyNormalItem implements StrategyItem{
 
     public void handle(Item item){
-        if(item.name.equals("Aged Brie")){
-            return;
+        if (item.quality > 0) {                    
+            item.quality -= 1;
+        }
+     
+        item.sellIn -= 1;            
+
+        if (item.sellIn < 0) {
+     
+            if (item.quality > 0) {  
+                item.quality -= 1;                           
+            }
         }
     }
 }
