c67e76f1b050181cb41a0746d6ff93be57b141d5
snake_761BD652B7 <snake@cyber-dojo.org>
2016-02-12 06:20:44 +0000

    72

--- sandbox/StrategyNormalItem.java
+++ sandbox/StrategyNormalItem.java
@@ -4,27 +4,24 @@ public class StrategyNormalItem implements StrategyItem{
         calculateQualityBeforeDecreasingSellIn();
         decreaseSellIn();
         calculateQualityAfterDecreasingSellIn();
+    }
 
+    protected void calculateQualityBeforeDecreasingSellIn() {
         if (item.quality > 0) {                    
             item.quality -= 1;
         }
-     
-        item.sellIn -= 1;            
+    }
 
+    protected void decreaseSellIn() {
+        item.sellIn -= 1;            
+    }
+
+    protected void calculateQualityAfterDecreasingSellIn() {
         if (item.sellIn < 0) {
      
             if (item.quality > 0) {  
                 item.quality -= 1;                           
             }
         }
-    }
-
-    protected void calculateQualityBeforeDecreasingSellIn() {
-    }
-
-    protected void decreaseSellIn() {
-    }
-
-    protected void calculateQualityAfterDecreasingSellIn() {
     }
 }
