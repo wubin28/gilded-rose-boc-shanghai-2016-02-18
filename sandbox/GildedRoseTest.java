6c54c31ca696f43875aa466a5a63b041140c4ef2
snake_761BD652B7 <snake@cyber-dojo.org>
2016-02-12 04:23:53 +0000

    10

--- sandbox/GildedRoseTest.java
+++ sandbox/GildedRoseTest.java
@@ -65,7 +65,17 @@ public class GildedRoseTest {
         assertEquals(80, app.items[0].quality);
     }
 
+    @Test
+    public void Sulfuras_quality_should_always_80_when_sellin_no_more_than_0() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 80) };
+        GildedRose app = new GildedRose(items);
 
+        app.updateQuality();
+
+        assertEquals("Sulfuras, Hand of Ragnaros", app.items[0].name);
+        assertEquals(0, app.items[0].sellIn);
+        assertEquals(80, app.items[0].quality);
+    }
 
 
 
