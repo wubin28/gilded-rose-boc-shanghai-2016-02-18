23a417d001dd274945e29a0d7d48e0a4f367edcd
snake_761BD652B7 <snake@cyber-dojo.org>
2016-02-12 05:40:41 +0000

    46

--- sandbox/StrategyFactory.java
+++ sandbox/StrategyFactory.java
@@ -1,6 +1,18 @@
 public class StrategyFactory{
     public static ItemStrategy create(String itemName){
-       if(itemName.equals("Sulfuras, Hand of Ragnaros"))
-           return new SulfurasStrategy();
+       if(itemName.equals("Sulfuras, Hand of Ragnaros")){
+           return new SulfurasStrategy();  
+        }
+
+        if(itemName.equals("Aged Brie")){
+           return new AgedBrieStrategy();  
+        }
+
+        if(itemName.equals("Backstage passes to a TAFKAL80ETC concert")){
+           return new BackstageStrategy();  
+        }
+        
+        return new NormalItemStrategy();  
+        
     }
 }
