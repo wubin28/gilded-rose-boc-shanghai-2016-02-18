bf670a805866e8e5e847632e6a8ec9de46431c8f
snake_761BD652B7 <snake@cyber-dojo.org>
2016-02-12 04:05:46 +0000

    3

--- sandbox/GildedRoseTest.java
+++ sandbox/GildedRoseTest.java
@@ -12,4 +12,14 @@ public class GildedRoseTest {
         assertEquals("fixme", app.items[0].name);
     }
 
+    @Test
+    public void normal_item_quality_should_decrease_by_1_if_sellin_greater_than_0() {
+        Item[] items = new Item[] { new Item("Normal item", 8, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Normal item", app.items[0].name);
+        assertEquals(7, app.items[0].sellIn);
+        assertEquals(9, app.items[0].quality);
+    }
+
 }
