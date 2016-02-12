bedcc4e35dc7613413112ae66777ab1547dadf43
snake_761BD652B7 <snake@cyber-dojo.org>
2016-02-12 04:57:06 +0000

    26

--- sandbox/GildedRose.java
+++ sandbox/GildedRose.java
@@ -26,6 +26,41 @@ class GildedRose {
                 continue;
             }
 
+            if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
+
+                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (items[i].sellIn < 11) {
+                            if (items[i].quality < 50) {
+                                items[i].quality = items[i].quality + 1;
+                            }
+                        }
+
+                        if (items[i].sellIn < 6) {
+                            if (items[i].quality < 50) {
+                                items[i].quality = items[i].quality + 1;
+                            }
+                        }
+                    }
+                items[i].sellIn = items[i].sellIn - 1;
+            
+
+            if (items[i].sellIn < 0) {
+         
+                if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (items[i].quality > 0) {  
+                        items[i].quality = items[i].quality - 1;                           
+                    }
+                } else {
+                    items[i].quality = items[i].quality - items[i].quality;
+                }
+                
+            }
+                continue;
+            }
+
+
 
             if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {                    
