9641e4351a64700074e15801e1e6dba47334d6a5
snake_761BD652B7 <snake@cyber-dojo.org>
2016-02-12 05:32:42 +0000

    43

--- sandbox/SulfurasStrategy.java
+++ sandbox/SulfurasStrategy.java
@@ -1,6 +1,8 @@
 public class SulfurasStrategy implements ItemStrategy{
 
     public void handle(Item item){
-    
+        if(item.name.equals("Sulfuras, Hand of Ragnaros")){
+            return;
+        }
     }
 }
