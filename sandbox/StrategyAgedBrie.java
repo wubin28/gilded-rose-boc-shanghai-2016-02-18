bda5a3203875ebea9a94f65ef37e3a033366efbf
snake_761BD652B7 <snake@cyber-dojo.org>
2016-02-12 05:54:02 +0000

    58

--- sandbox/StrategyAgedBrie.java
+++ sandbox/StrategyAgedBrie.java
@@ -1,8 +1,17 @@
 public class StrategyAgedBrie implements StrategyItem{
 
     public void handle(Item item){
-        if(item.name.equals("Aged Brie")){
-            return;
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+
+        item.sellIn = item.sellIn - 1;
+    
+        if (item.sellIn < 0) {
+           
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+            }                   
         }
     }
 }
